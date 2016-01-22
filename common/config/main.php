ff2ee21967e2c75202245dc6176a2388003c180d
bscheshirwork <bscheshir.work@gmail.com>
2016-01-22 15:27:58 +0300

    Update main.php

--- common/config/main.php
+++ common/config/main.php
@@ -12,57 +12,6 @@ return [
     'modules' => [
         'user' => [
             'class' => 'dektrium\user\Module',
-            'controllerMap' => [
-                'admin' => [
-                    'class' => 'dektrium\user\controllers\AdminController',
-                    'as access' => [
-                        'class' => 'yii\filters\AccessControl',
-                        'rules' => [
-                            [
-                                'allow' => true,
-                                'roles' => ['administrateUser'],
-                            ],
-                        ],
-                    ],
-                ],
-                'profile' => [
-                    'class' => 'dektrium\user\controllers\ProfileController',
-                    'as access' => [
-                        'class' => 'yii\filters\AccessControl',
-                        'rules' => [
-                            [
-                                ['allow' => true, 'actions' => ['index'], 'roles' => ['@']], //redirect на show. id в данном действии нет
-                                ['allow' => true, 'actions' => ['show'], 'roles' => ['showUserProfile']], // просмотр других запрещён
-                            ],
-                        ],
-                    ],
-                ],
-                'settings' => [
-                    'class' => 'dektrium\user\controllers\SettingsController',
-                    'as access' => [
-                        'class' => 'yii\filters\AccessControl',
-                        'rules' => [
-                            [
-                                ['allow' => true, 'actions' => ['confirm'], 'roles' => ['@']], //подтвердить почту по ссылке можно только залогинившись
-                                ['allow' => true, 'actions' => ['account'], 'roles' => ['updateSelfAccount']],
-                                ['allow' => true, 'actions' => ['profile'], 'roles' => ['updateSelfProfile']],
-                            ],
-                        ],
-                    ],
-                ],
-            ],
-        ],
-        'admin' => [
-            'class' => 'mdm\admin\Module',
-            'as access' => [
-                'class' => 'yii\filters\AccessControl',
-                'rules' => [
-                    [
-                        'allow' => true,
-                        'roles' => ['administrateRbac'],
-                    ],
-                ],
-            ],
         ],
     ],
 ];
